1e68d7b9390df1298b9c916ab817b41b59caeb98
byAbbadon <kasiopess@abv.bg>
2020-03-29 00:38:59 +0200

    add folder 8.2 solutions

---
+++ 8.2 Regular Expressions - More Exercise/4. Santa's Secret Helper.js
@@ -0,0 +1,36 @@
+function santaSecretHelper(inputArr) {
+  let arr = [...inputArr]
+  let key = arr.shift()
+  let word = arr.shift()
+
+  let pattern = /\@(?<name>[A-z]+)[^\@\-\!\:\>]*\!(?<status>[G])\!/g
+
+  while (word !== 'end') {
+    let decryption = [...word].map(el => String.fromCharCode(el.charCodeAt() - key)).join('')
+    let result = [...decryption.matchAll(pattern)]
+
+    if (result[0] !== undefined) {
+      console.log(result[0].groups.name);
+    }
+
+    word = arr.shift()
+  }
+}
+
+// santaSecretHelper([
+//   "4",
+//   "~lwzjkl~jenlymfDFsffmiCwozwlzjln%K%",
+//   "0zfjrl}xnrlDWeqqmi/wnznlwzjnn%K%onhfhnf",
+//   ";:<lyiljz{onzDPere=;=9<;8=rhknlf%K%",
+//   "Wonvfkmwzkmpwvzkm'lhjnlDWeqerxle0wlnzj{nz%K%nohwn",
+//   "DReh}e=<4lhzj1%K%",
+//   "end",
+// ])
+
+// santaSecretHelper([
+//   '3',
+//   'CNdwhamigyenumje$J$',
+//   'CEreelh-nmguuejnW$J$',
+//   'CVwdq&gnmjkvng$Q$',
+//   'end',
+// ])
