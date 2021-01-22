bb1607d9ec163e5ea63a7fbcd274c910c1ed3f31
byAbaddon <51271834+byAbaddon@users.noreply.github.com>
2021-01-22 12:10:16 +0200

    Update 08. Array Manipulations.js

--- 3.3 Arrays Advanced - Lab/08. Array Manipulations.js
+++ 3.3 Arrays Advanced - Lab/08. Array Manipulations.js
@@ -1,3 +1,22 @@
+function arrayManipulations(arr) {
+  let listOfNum = arr.shift().split(' ').map(Number)
+
+  for (let i = 0; i < arr.length; i++) {
+    let [command, num, index] = arr[i].split(' ')
+    if (command == 'Add') listOfNum.push(num)
+    if (command == 'Remove') listOfNum = listOfNum.filter(el => el !== +num)
+    if (command == 'RemoveAt') listOfNum.splice(num, 1)
+    if (command == 'Insert') listOfNum.splice(index, 0, num)
+  }
+
+  return listOfNum.join(' ')
+}
+
+
+//console.log(arrayManipulations(['4 19 2 53 6 43', 'Add 3', 'Remove 2', 'RemoveAt 1', 'Insert 8 3']))
+
+//---------------------------------------------------------------------(2)------------------------------------------------------
+
 function arrayManipulations(inputArr) {
   let arr = [...inputArr]
   let listOfNum = arr.shift().split(' ').map(Number)
