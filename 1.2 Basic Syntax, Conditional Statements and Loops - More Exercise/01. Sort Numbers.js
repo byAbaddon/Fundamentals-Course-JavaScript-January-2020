481fdf4a8fc92e84249d8dad8473debf899a0a74
byAbaddon <51271834+byAbaddon@users.noreply.github.com>
2021-01-25 00:46:08 +0200

    Update 01. Sort Numbers.js

--- 1.2 Basic Syntax, Conditional Statements and Loops - More Exercise/01. Sort Numbers.js
+++ 1.2 Basic Syntax, Conditional Statements and Loops - More Exercise/01. Sort Numbers.js
@@ -1,8 +1,5 @@
-function sortNumbers(a, b, c) {
-  let arr = [a, b, c]
-  arr.sort((a, b) => b - a).forEach(el => {
-    console.log(el)
-  })
+function sortNumbers(...arr) {
+  arr.sort((a, b) => b - a).forEach(el => console.log(el))
 }
 
 // sortNumbers(-2, 1, 3)
