acf61292de74f79cbd815249f2648a44e671a5a8
byAbbadon <kasiopess@abv.bg>
2021-02-06 01:41:19 +0200

    fix

--- 6.1 Associative Arrays - Exercise/06. Travel Time.js
+++ 6.1 Associative Arrays - Exercise/06. Travel Time.js
@@ -1,29 +1,23 @@
-function travelTime(inputArr) {
-  let arr = [...inputArr]
+function travelTime(arr) {
   let objTravel = {}
 
   while (arr.length > 0) {
     let [country, city, price] = arr.shift().split(' > ')
 
-    if (!objTravel.hasOwnProperty(country)) {
+    if (!objTravel.hasOwnProperty(country))
       objTravel[country] = {}
-    }
 
-    if (!objTravel[country].hasOwnProperty(city)) {
+    if (!objTravel[country].hasOwnProperty(city))
       objTravel[country][city] = price
-    } else {
-      if (objTravel[country][city] > price) {
-        objTravel[country][city] = price
-      }
-    }
-
+    else
+    if (objTravel[country][city] > price)
+      objTravel[country][city] = price
   }
 
   objTravel = Object.entries(objTravel).sort((a, b) => a[0].localeCompare(b[0]))
 
-  for (const [key, valKey] of objTravel) {
+  for (const [key, valKey] of objTravel)
     console.log(key + ' ->', Object.entries(valKey).map(city => city.join(' -> ')).join(' '))
-  }
 }
 
 // travelTime(
--- 6.1 Associative Arrays - Exercise/07. Company Users.js
+++ 6.1 Associative Arrays - Exercise/07. Company Users.js
@@ -1,34 +1,28 @@
-function companyUsers(inputArr) {
-  let arr = [...inputArr]
+function companyUsers(arr) {
   let objCompany = {}
 
   while (arr.length > 0) {
     let [company, id] = arr.shift().split(' -> ')
-    if (!objCompany.hasOwnProperty(company)) {
+    if (!objCompany.hasOwnProperty(company))
       objCompany[company] = new Array(id)
-    } else {
-      if (!objCompany[company].includes(id)) {
-         objCompany[company].push(id)
-      }
-     
+    else {
+      if (!objCompany[company].includes(id))
+         objCompany[company].push(id) 
     }
-
   }
 
   objCompany = Object.entries(objCompany).sort((a, b) => a[0].localeCompare(b[0]))
   for (const [key, value] of objCompany) {
-    console.log(key);
-    value.forEach(el => {
-      console.log('--', el);
-    });
+    console.log(key)
+    value.forEach(el => console.log('--', el))
   }
 }
 
-companyUsers(
-  [
-    'SoftUni -> AA12345',
-    'SoftUni -> CC12344',
-    'Lenovo -> XX23456',
-    'SoftUni -> AA12345',
-    'Movement -> DD11111'
-    ])
+// companyUsers(
+//   [
+//     'SoftUni -> AA12345',
+//     'SoftUni -> CC12344',
+//     'Lenovo -> XX23456',
+//     'SoftUni -> AA12345',
+//     'Movement -> DD11111'
+//     ])
