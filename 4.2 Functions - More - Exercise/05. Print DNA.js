df6fe400e30663ad123484d48406d91bfc2c3207
byAbbadon <kasiopess@abv.bg>
2021-02-11 13:06:22 +0200

    fix

--- 4.2 Functions - More - Exercise/05. Print DNA.js
+++ 4.2 Functions - More - Exercise/05. Print DNA.js
@@ -1,28 +1,19 @@
 function printDNA(input) {
-  let dna = [...'ATCGTTAGGG'.repeat(100).split('')]
+  let dna = [...'ATCGTTAGGG'.repeat(10).split('')]
   let loop = 0
 
   while (input--) {
     loop === 4 ? loop = 0 : loop
 
     switch (loop) {
-      case 0:
-        console.log(`**${dna.shift()}${dna.shift()}**`);
-        break;
-      case 1:
-        console.log(`*${dna.shift()}--${dna.shift()}*`);
-        break;
-      case 2:
-        console.log(`${dna.shift()}----${dna.shift()}`);
-        break;
-      case 3:
-        console.log(`*${dna.shift()}--${dna.shift()}*`);
-        break;
+      case 0: console.log(`**${dna.shift()}${dna.shift()}**`); break
+      case 1: console.log(`*${dna.shift()}--${dna.shift()}*`); break
+      case 2: console.log(`${dna.shift()}----${dna.shift()}`); break
+      case 3: console.log(`*${dna.shift()}--${dna.shift()}*`); break
     }
     
     loop++
   }
 }
-
 
 // printDNA(29)
