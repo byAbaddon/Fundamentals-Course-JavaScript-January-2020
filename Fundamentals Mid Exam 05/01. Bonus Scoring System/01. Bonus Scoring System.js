9afaddeaa53d861ad09fc24da06dc96b63a12842
byAbbadon <kasiopess@abv.bg>
2021-02-13 00:03:29 +0200

    fix

--- Fundamentals Mid Exam 05/01. Bonus Scoring System/01. Bonus Scoring System.js
+++ Fundamentals Mid Exam 05/01. Bonus Scoring System/01. Bonus Scoring System.js
@@ -1,4 +1,28 @@
 function bonusScoringSystem(arr) {
+    let [numberOfStudents, courseLectures, currentBonus] = arr.splice(0, 3).map(Number)
+    let gradesObj = {'bonus': 0, 'visited': 0}
+  
+  for (const studentAttendances of arr) {
+      let totalBonus = Math.round(studentAttendances / courseLectures * (5 + currentBonus))
+      if (totalBonus > gradesObj['bonus']) {
+        gradesObj['bonus'] = totalBonus
+        gradesObj['visited'] = studentAttendances
+      }
+    }
+    return `Max Bonus: ${gradesObj['bonus']}.\nThe student has attended ${gradesObj['visited']} lectures.`
+  }
+  
+  
+//   console.log(
+//     bonusScoringSystem([
+//       '5', '25', '30',
+//       '12', '19', '24',
+//       '16', '20'
+//     ]))
+
+//-------------------------------------------------------(2)------------------------
+
+function bonusScoringSystem(arr) {
     arr = arr.map(Number)
     let numberOfStudents = arr.shift()
     let courseLectures = arr.shift()
