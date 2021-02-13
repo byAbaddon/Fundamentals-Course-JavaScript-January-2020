1345416add5576a6f3ecf77f7f1920e652eee345
byAbbadon <kasiopess@abv.bg>
2021-02-13 18:04:48 +0200

    fix

--- 5.1 Objects and Classes - Lab/02. City.js
+++ 5.1 Objects and Classes - Lab/02. City.js
@@ -1,10 +1,23 @@
-function city(...inputArr) {
-  let arr = [...inputArr]
-  let objInfo = {name : arr[0], area : arr[1], population : arr[2], country : arr[3], postCode : arr[4]}
+function city(inputObj) {
+  for (const key in inputObj) console.log(key + ' -> ' + inputObj[key])
+}
 
- for (const key in objInfo) {
-   console.log( key + ' -> '+ objInfo[key]);
-  }
+// city({
+//   name: "Sofia",
+//   area: 492,
+//   population: 1238438,
+//   country: "Bulgaria",
+//   postCode: "1000"
+// })
+
+
+
+//=============================== old solution not work in Judge
+function city(...arr) {
+  let objInfo = {name : arr[0], area : arr[1], population : arr[2], country : arr[3], postCode : arr[4]}
+  for (const key in objInfo) console.log(key + ' -> ' + objInfo[key]);
 }
 
 //city("Sofia"," 492", "1238438", "Bulgaria", "1000")
+
+
