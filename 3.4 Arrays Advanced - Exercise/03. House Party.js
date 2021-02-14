1aec62b1029a1894b777c443741a9a9614f2b1c9
byAbbadon <kasiopess@abv.bg>
2021-02-14 22:24:31 +0200

    fix

--- 3.4 Arrays Advanced - Exercise/03. House Party.js
+++ 3.4 Arrays Advanced - Exercise/03. House Party.js
@@ -1,7 +1,32 @@
+function houseParty(guests) {
+  let partyList = new Set()
+  for (const token of guests) {
+    const [name, , status] = token.split(' ')
+
+    if (!partyList.has(name) && status == 'going!')
+      partyList.add(name)
+    else if (!partyList.has(name) && status == 'not') console.log(`${name} is not in the list!`)
+    else {
+      if (partyList.has(name) && status == 'going!') console.log(`${name} is already in the list!`)
+      else
+        partyList.delete(name)
+    }
+  }
+
+  for (const el of partyList) console.log(el)
+}
+
+
+//houseParty(['Allie is going!', 'George is going!', 'John is not going!', 'George is not going!'])
+//houseParty(['Tom is going!', 'Annie is going!', 'Tom is going!', 'Garry is going!', 'Jerry is going!'])
+
+
+//-------------------------------------------------(2)------------------------------------
+
 function houseParty(arr) {
   let partyList = []
   let name = isGoing = ''
-  
+
   for (let i = 0; i < arr.length; i++) {
     name = arr[i].split(' ')[0]
     isGoing = arr[i].split(' ').slice(1, )
@@ -16,7 +41,7 @@ function houseParty(arr) {
       if (partyList.includes(name)) {
         let findIndexName = partyList.lastIndexOf(name)
         partyList.splice(findIndexName, 1)
-      } else{
+      } else {
         console.log(`${name} is not in the list!`);
       }
 
@@ -28,5 +53,7 @@ function houseParty(arr) {
   }
 }
 
-//houseParty(['Allie is going!', 'George is going!', 'John is not going!', 'George is not going!'])
-//houseParty(['Tom is going!', 'Annie is going!', 'Tom is going!', 'Garry is going!', 'Jerry is going!'])
+
+
+// houseParty(['Allie is going!', 'George is going!', 'John is not going!', 'George is not going!'])
+// houseParty(['Tom is going!', 'Annie is going!', 'Tom is going!', 'Garry is going!', 'Jerry is going!'])
