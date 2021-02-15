e2e8e7897bfb9039cfb78871cc591306dfb7cce6
byAbbadon <kasiopess@abv.bg>
2021-02-15 20:16:35 +0200

    add

--- 7.1 Text Processing - Exercise/01. Reveal Words.js
+++ 7.1 Text Processing - Exercise/01. Reveal Words.js
@@ -1,4 +1,19 @@
 function revealWords(...input) {
+  const [words, text, finalText] = [input[0].split(', '), input[1].split(' '), []]
+  for (let i = 0; i < text.length; i++) {
+    if (text[i].includes('*'))  words.map(w => w.length == text[i].length ? text[i] = w : null)   
+    finalText.push(text[i])
+  }
+  return finalText.join(' ')
+}
+
+// console.log(revealWords('great, learning', 'softuni is ***** place for ******** new programming languages'))
+
+
+//----------------------------------------------(2)-------------------------------------
+
+
+function revealWords(...input) {
   let arrWords = input[0]
   let text = input[1].split(' ')
   let collection = ''
@@ -15,4 +30,4 @@ function revealWords(...input) {
   return collection
 }
 
-//console.log(revealWords('great, learning', 'softuni is ***** place for ******** new programming languages'))
+// console.log(revealWords('great, learning', 'softuni is ***** place for ******** new programming languages'))
