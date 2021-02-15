1b2676b8cbd232185ba6ca7cfa43cf494f8a9101
byAbbadon <kasiopess@abv.bg>
2021-02-15 02:38:39 +0200

    fix

--- 3.4 Arrays Advanced - Exercise/03. House Party.js
+++ 3.4 Arrays Advanced - Exercise/03. House Party.js
@@ -11,7 +11,7 @@ function houseParty(guests) {
 }
 
 
-houseParty(['Allie is going!', 'George is going!', 'John is not going!', 'George is not going!'])
+// houseParty(['Allie is going!', 'George is going!', 'John is not going!', 'George is not going!'])
 //houseParty(['Tom is going!', 'Annie is going!', 'Tom is going!', 'Garry is going!', 'Jerry is going!'])
 
 
