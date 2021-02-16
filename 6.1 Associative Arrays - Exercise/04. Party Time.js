e733c53cb771c24b89e849faff368168b0cf76e2
byAbbadon <kasiopess@abv.bg>
2021-02-16 14:48:52 +0200

    fix

--- 6.1 Associative Arrays - Exercise/04. Party Time.js
+++ 6.1 Associative Arrays - Exercise/04. Party Time.js
@@ -6,34 +6,30 @@ function partyTime(arr) {
 
   for (let el of arrivedGuests) {
     if (invitedGuests.includes(el)) {
-      let index = invitedGuests.indexOf(el);
-      invitedGuests.splice(index, 1);
+      let index = invitedGuests.indexOf(el)
+      invitedGuests.splice(index, 1)
     }
   }
 
   for (let el of invitedGuests) {
-    if (/\d/gim.test(el[0])) {
-      listVIPGuest.push(el);
-    } else {
-      listRegularGuest.push(el);
-    }
+    if (/\d/gim.test(el[0])) listVIPGuest.push(el)
+    else listRegularGuest.push(el)
   }
 
-  console.log(listVIPGuest.length + listRegularGuest.length);
-  console.log(listVIPGuest.join("\n"));
-  console.log(listRegularGuest.join("\n"));
+  console.log(listVIPGuest.length + listRegularGuest.length)
+  console.log(listVIPGuest.join('\n'))
+  console.log(listRegularGuest.join('\n'))
 }
 
-// partyTime(
-//   ['7IK9Yo0h',
-//     '9NoBUajQ',
-//     'Ce8vwPmE',
-//     'SVQXQCbc',
-//     'tSzE5t0p',
-//     'PARTY',
-//     '9NoBUajQ',
-//     'Ce8vwPmE',
-//     'SVQXQCbc',
+partyTime(
+  ['7IK9Yo0h',
+    '9NoBUajQ',
+    'Ce8vwPmE',
+    'SVQXQCbc',
+    'tSzE5t0p',
+    'PARTY',
+    '9NoBUajQ',
+    'Ce8vwPmE',
+    'SVQXQCbc',
 
-//   ])
-
+  ])
