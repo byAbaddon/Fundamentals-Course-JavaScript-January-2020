f43a58d3b365dbe4c108080fbfa2d9531132861a
byAbbadon <kasiopess@abv.bg>
2021-02-18 16:12:19 +0200

    fix

--- 7.1 Text Processing - Exercise/08. Hard Word.js
+++ 7.1 Text Processing - Exercise/08. Hard Word.js
@@ -11,7 +11,25 @@ function hardWords(input) {
 //   ['pie', 'bring', 'glad', 'During', 'amazing', 'pharmacist', 'sprained']
 // ])
 
-//-------------------------------------------------------(2)---------------------------------------------
+
+//-------------------------------------------------------(2)--------------------------------------------
+function hardWords(input) {
+  let arrText = input[0].split(' ').map(w => {
+    if (/_+/g.test(w)) return w.replace(/_+/g, input[1].filter(word => word.length == w.length))
+    return w
+  })
+
+  return arrText.join(' ')
+}
+
+
+// console.log(
+//   hardWords(['Hi, grandma! I\'m so ____ to write to you. ______ the winter vacation, so _______ things happened. My dad bought me a sled. Mom started a new job as a __________. My brother\'s ankle is ________, and now it bothers me even more. Every night Mom cooks ___ on your recipe because it is the most delicious. I hope this year Santa will _____ me a robot.',
+//     ['pie', 'bring', 'glad', 'During', 'amazing', 'pharmacist', 'sprained']
+//   ]))
+
+
+//-------------------------------------------------------(3)---------------------------------------------
 
 function hardWords(inputArr) {
   let arrText = inputArr[0].split(' ')
