3b28143db653d3cc3e7f59bcac655dd387cfeb7c
byAbbadon <kasiopess@abv.bg>
2021-02-25 22:55:57 +0200

    fix

--- Fundamentals Mid Exam Retake 03/01. Counter Strike/01. Counter Strike.js
+++ Fundamentals Mid Exam Retake 03/01. Counter Strike/01. Counter Strike.js
@@ -1,4 +1,27 @@
 function counterStrike(arr) {
+  let energy = +arr.shift()
+  let countWon = 0
+
+  while (arr.length > 0) {
+    let enemy = +arr.shift()
+
+    if (isNaN(enemy)) break
+    if (energy < enemy) return `Not enough energy! Game ends with ${countWon} won battles and ${energy} energy`
+
+    energy -= enemy
+    countWon++
+
+    if (countWon % 3 == 0) energy += countWon
+  }
+
+  return `Won battles: ${countWon}. Energy left: ${energy}`
+}
+
+// console.log(counterStrike(['100', '10', '10', '10', '1', '2', '3', '73', '10']))
+// console.log(counterStrike(['200', '54', '14', '28', '13', 'End of battle']))
+
+//---------------------------------------------------------(2)------------------------------
+function counterStrike(arr) {
   arr = arr.map(el => isNaN(el) ? el : Number(el))
   let energy = +arr.shift()
   let countWon = 0
