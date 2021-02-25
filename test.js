099ee60484d0db74d4e8158802edc04124c0abaa
byAbbadon <kasiopess@abv.bg>
2021-02-25 22:54:58 +0200

    fix

---
+++ test.js
@@ -0,0 +1,21 @@
+function counterStrike(arr) {
+  let energy = +arr.shift()
+  let countWon = 0
+
+  while (arr.length > 0) {
+    let enemy = +arr.shift()
+
+    if (isNaN(enemy)) break
+    if (energy < enemy) return `Not enough energy! Game ends with ${countWon} won battles and ${energy} energy`
+
+    energy -= enemy
+    countWon++
+
+    if (countWon % 3 == 0) energy += countWon
+  }
+
+  return `Won battles: ${countWon}. Energy left: ${energy}`
+}
+
+// console.log(counterStrike(['100', '10', '10', '10', '1', '2', '3', '73', '10']))
+// console.log(counterStrike(['200', '54', '14', '28', '13', 'End of battle']))
