95923c5b6afce3fb56fa2aed4873995c1e44d251
byAbbadon <kasiopess@abv.bg>
2021-03-13 14:42:38 +0200

    fix

--- 8.1 Regular Expressions - Exercise/06. Extract Emails.js
+++ 8.1 Regular Expressions - Exercise/06. Extract Emails.js
@@ -1,7 +1,7 @@
 function extractEmails(text) {
-  let pattern = /(?<=\s)([A-Za-z\d]+[\.\-\_]?[A-Za-z\d]+)+@\w+\-?\w+(\.\w+\-?\w+)+\b/gm
+  let pattern = /(?<=\s)([a-z\d]+[\.\-\_]?[a-z\d]+)+@\w+\-?\w+(\.\w+\-?\w+)+\b/gm
   let result = text[0].match(pattern)
   if (result != null) result.forEach(el => console.log(el))  
 }
 
-// extractEmails(['Just send email to s.miller@mit.edu and j.hopking@york.ac.uk for more information.'])
+//extractEmails(['Just send email to s.miller@mit.edu and j.hopking@york.ac.uk for more information.'])
