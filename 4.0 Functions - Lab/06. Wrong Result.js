9cc5b5a59673a14d8063d03c4189da55f29ccbb2
byAbbadon <kasiopess@abv.bg>
2021-03-15 00:25:52 +0200

    fix

--- 4.0 Functions - Lab/06. Wrong Result.js
+++ 4.0 Functions - Lab/06. Wrong Result.js
@@ -1,7 +1,6 @@
-function wrongResult(num1, num2, num3) {
-  let sum = num1 * num2 * num3
-  return sum < 0 ? 'Negative' : 'Positive'
+function signCheck(num1, num2, num3) {
+  return num1 * num2 * num3  < 0 ? 'Negative' : 'Positive'
 }
 
-//console.log(wrongResult(5, 12, -15))
-//console.log(wrongResult(-1, 0, 1))
+//console.log(signCheck(5, 12, -15))
+//console.log(signCheck(-1, 0, 1))
