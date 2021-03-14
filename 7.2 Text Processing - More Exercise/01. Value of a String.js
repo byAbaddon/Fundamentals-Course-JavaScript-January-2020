5653029461b35dc05727bb8552e9f4a4af176872
byAbbadon <kasiopess@abv.bg>
2021-03-14 22:35:12 +0200

    fix

--- 7.2 Text Processing - More Exercise/01. Value of a String.js
+++ 7.2 Text Processing - More Exercise/01. Value of a String.js
@@ -1,3 +1,17 @@
+function valueOfString([str, command]) {
+  command == 'LOWERCASE' ?
+    str = str.match(/[a-z]/g).map(el => el.toLowerCase().charCodeAt(0)) :
+    str = str.match(/[A-Z]/g).map(el => el.toUpperCase().charCodeAt(0))
+
+  return `The total sum is: ${str.reduce((a, b) => a + b)}`
+}
+
+// console.log(valueOfString(['HelloFromMyAwesomePROGRAM', 'LOWERCASE']))
+// console.log(valueOfString(['AC/DC', 'UPPERCASE']))
+
+
+//------------------------------------------------(2)-----------------------------------
+
 function valueOfString(arr) {
   let [str, command] = [...arr]
  
--- test.js
+++ test.js
@@ -1,26 +1,38 @@
-function partyTime(arr) {
-  let invitedGuests = arr.splice(0, arr.indexOf('PARTY'))
-  let arrivedGuests = arr.splice(1, Infinity)
+function valueOfString([str, command]) {
+  command == 'LOWERCASE' ?
+    str = str.match(/[a-z]/g).map(el => el.toLowerCase().charCodeAt(0)) :
+    str = str.match(/[A-Z]/g).map(el => el.toUpperCase().charCodeAt(0))
 
-  for (let el of arrivedGuests)
-    if (invitedGuests.includes(el))
-      invitedGuests.splice(invitedGuests.indexOf(el), 1)
-
-  let vipGuest = invitedGuests.filter(el => (/\d/.test(el[0])))
-  let regularGuest = invitedGuests.filter(el => !(/\d/.test(el[0])))
-
-  console.log(vipGuest.length + regularGuest.length)
-  console.log(`${vipGuest.join('\n')}\n${regularGuest.join('\n')}`)
+  return `The total sum is: ${str.reduce((a, b) => a + b)}`
 }
 
-partyTime(
-  ['7IK9Yo0h',
-    '9NoBUajQ',
-    'Ce8vwPmE',
-    'SVQXQCbc',
-    'tSzE5t0p',
-    'PARTY',
-    '9NoBUajQ',
-    'Ce8vwPmE',
-    'SVQXQCbc',
-  ])
+// console.log(valueOfString(['HelloFromMyAwesomePROGRAM', 'LOWERCASE']))
+// console.log(valueOfString(['AC/DC', 'UPPERCASE']))
+
+
+function valueOfString([str, arg1]) {
+
+  let result = 0;
+
+  if (arg1 === `UPPERCASE`) {
+    for (const char of str) {
+      let code = char.charCodeAt();
+      if (code >= 65 && code <= 90) {
+        result += Number(code);
+      }
+    }
+  }
+  if (arg1 === `LOWERCASE`) {
+    for (const char of str) {
+      let code = char.charCodeAt();
+      if (code >= 97 && code <= 122) {
+        result += Number(code);
+      }
+    }
+  }
+  console.log(`The total sum is: ${result}`);
+}
+
+
+// valueOfString(['HelloFromMyAwesomePROGRAM', 'LOWERCASE'])
+// valueOfString(['AC/DC', 'UPPERCASE'])
