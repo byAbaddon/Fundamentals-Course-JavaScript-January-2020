616cad8daf5414588afc64fc6cc0320c6f0ac3d9
byAbbadon <kasiopess@abv.bg>
2021-03-19 17:15:07 +0200

    fix

--- 8.2 Regular Expressions - More Exercise/4. Santa's Secret Helper.js
+++ 8.2 Regular Expressions - More Exercise/4. Santa's Secret Helper.js
@@ -6,7 +6,7 @@ function santaSecretHelper(arr) {
   for (const word of arr) {
     let decryption = [...word].map(el => String.fromCharCode(el.charCodeAt() - key)).join('')
     let result = [...decryption.matchAll(pattern)]
-    if (result[0] !== undefined)  /^/.test(result[0]) ? console.log( String(result[0][1]).match(/^\w+\b/)[0]) : null    
+    if (result[0] !== undefined)  console.log(String(result[0][1]).match(/^\w+\b/)[0])         
   }
 }
 
