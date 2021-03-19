9b4416163a65332a7cc4074722cb3389b134f966
byAbbadon <kasiopess@abv.bg>
2021-03-19 13:58:20 +0200

    fix

--- 8.2 Regular Expressions - More Exercise/4. Santa's Secret Helper.js
+++ 8.2 Regular Expressions - More Exercise/4. Santa's Secret Helper.js
@@ -1,5 +1,32 @@
-function santaSecretHelper(inputArr) {
-  let arr = [...inputArr]
+function santaSecretHelper(arr) {
+  arr.pop()
+  let key = arr.shift()
+  let pattern = /\@(?<name>[A-z]+)[^\@\-\!\:\>]*\!(?<status>[G])\!/g
+
+  for (const word of arr) {
+    let decryption = [...word].map(el => String.fromCharCode(el.charCodeAt() - key)).join('')
+    let result = [...decryption.matchAll(pattern)]
+    if (result[0] !== undefined)  console.log(result[0].groups.name) 
+  }
+}
+
+
+// santaSecretHelper([
+//   "4",
+//   "~lwzjkl~jenlymfDFsffmiCwozwlzjln%K%",
+//   "0zfjrl}xnrlDWeqqmi/wnznlwzjnn%K%onhfhnf",
+//   ";:<lyiljz{onzDPere=;=9<;8=rhknlf%K%",
+//   "Wonvfkmwzkmpwvzkm'lhjnlDWeqerxle0wlnzj{nz%K%nohwn",
+//   "DReh}e=<4lhzj1%K%",
+//   "end",
+// ])
+
+
+//----------------------------------------(2)-------------------------------------------
+
+
+
+function santaSecretHelper(arr) {
   let key = arr.shift()
   let word = arr.shift()
 
