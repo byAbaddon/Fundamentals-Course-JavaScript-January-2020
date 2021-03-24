bade123b458162f2724eaf3eb22fedf95ff58965
byAbbadon <kasiopess@abv.bg>
2021-03-24 17:19:24 +0200

    fix

--- 8.1 Regular Expressions - Exercise/02. Race.js
+++ 8.1 Regular Expressions - Exercise/02. Race.js
@@ -1,22 +1,19 @@
 function race(input) {
-  let playersName = input.shift().split(', ');
-  let objRanking = {};
+  input.pop()
+  let playersName = input.shift().split(', ')
+  let objRanking = {}
   let name = ''
   let points = 0
-  let loop = input.shift();
 
-  while (loop !== 'end of race') {
+  for (loop of input) {
+    name = loop.match(/[A-Za-z]/gm).join('')
+    points = loop.match(/\d/gm).reduce((a, b) => (+a + +b))
 
-    name = loop.match(/[A-Za-z]/gm).join('');
-    points = loop.match(/\d/gm).reduce((a, b) => (+a + +b));
-
-    if (!objRanking[name] && playersName.includes(name)) {
-      objRanking[name] = points;
-    } else if (objRanking[name]) {
-      objRanking[name] += points;
-    }
-    
-    loop = input.shift();
+    if (!objRanking[name] && playersName.includes(name))
+      objRanking[name] = points     
+    else if (objRanking[name])
+      objRanking[name] += points
+     
   }
 
   objRanking = Object.entries(objRanking).sort((a, b) => b[1] - a[1])
@@ -26,22 +23,22 @@ function race(input) {
 
   for (const key of objRanking) {
     if (count < 3) {
-      console.log(finishRank[count], key[0]);
+      console.log(finishRank[count], key[0])
       count++
     }
   }
 }
 
-race([
-  'George, Peter, Bill, Tom',
-  'G4e@55or%6g6!68e!!@',
-  'R1@!3a$y4456@',
-  'B5@i@#123ll',
-  'G@e54o$r6ge#',
-  '7P%et^#e5346r',
-  'T$o553m&6',
-  'end of race'
-])
+// race([
+//   'George, Peter, Bill, Tom',
+//   'G4e@55or%6g6!68e!!@',
+//   'R1@!3a$y4456@',
+//   'B5@i@#123ll',
+//   'G@e54o$r6ge#',
+//   '7P%et^#e5346r',
+//   'T$o553m&6',
+//   'end of race'
+// ])
 
 //1st place: George
 //2nd place: Peter
