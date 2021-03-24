50ee6a1c1e4036e4db579f35835da16ae9e08b19
byAbbadon <kasiopess@abv.bg>
2021-03-24 22:17:19 +0200

    fix

--- 7.1 Text Processing - Exercise/02. Modern Times of #(HashTag).js
+++ 7.1 Text Processing - Exercise/02. Modern Times of #(HashTag).js
@@ -1,9 +1,6 @@
 function modernTimes(input) {
-  for (const el of input.split(' ')) {
-    if (el.match(/(#[A-z]+)/gim)) {
-      console.log(el.substring(1, ))
-    }
-  }
+  for (const el of input.split(' '))
+    if (el.match(/(#[A-z]+)/gim)) console.log(el.substring(1, ))
 }
 
 //modernTimes('Nowadays everyone uses # to tag a #special word in #socialMedia')
