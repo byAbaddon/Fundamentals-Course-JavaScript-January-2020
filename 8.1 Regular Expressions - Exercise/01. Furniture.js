f73c7895ebed25cbe2dd5d151d3ad48efe7306fe
byAbbadon <kasiopess@abv.bg>
2021-03-25 14:16:03 +0200

    fix

--- 8.1 Regular Expressions - Exercise/01. Furniture.js
+++ 8.1 Regular Expressions - Exercise/01. Furniture.js
@@ -1,5 +1,31 @@
 function furniture(arr) {
-  let pattern =  />>(?<name>[A-Z]+[a-z]*)<<(?<price>\d+[\.|\d]\d*)!(?<count>\d+)/
+  arr.pop()
+  let pattern = />>(?<name>\w+)<<(?<price>\d+\.?\d+)!(?<quantity>\d+)/
+  let sum = 0
+
+  console.log('Bought furniture:')
+  for (const el of arr) {
+    let result = el.match(pattern)
+    if (result) {
+      let items = result.groups.name
+      let price = result.groups.price
+      let count = result.groups.quantity
+
+      sum += price * count
+      console.log(items)
+    }
+  }
+  console.log(`Total money spend: ${sum.toFixed(2)}`)
+}
+
+// furniture(['>>TV<<300!5', '>Invalid<<!5', 'Purchase', ])
+// furniture(['>>TV<<300!5', '>>Sofa<<312.23!3', '>Invalid<<!5', 'Purchase', ])
+
+
+//------------------------------------------------(2)----------------------------------------
+
+function furniture(arr) {
+  let pattern = />>(?<name>[A-Z]+[a-z]*)<<(?<price>\d+[\.|\d]\d*)!(?<count>\d+)/
   let sum = 0
   let arrBoughtFurniture = []
 
--- test.js
+++ test.js
@@ -1,24 +1,49 @@
-function garage(arr) {
-  let garagesObj = {}
+function systemsRegister(input) {
+  let systems = {}
 
-  for (const el of arr) {
-    let [garageNum, carList] = el.split(' - ')
-
-    if (!garagesObj[garageNum]) {
-      garagesObj[garageNum] = []
-    }
-    garagesObj[garageNum].push(carList)
+  for (line of input) {
+    let [sys, component, name] = line.split(' | ')
+    if (!systems[sys]) systems[sys] = {}
+    if (!(systems[sys][component])) systems[sys][component] = []
+    if (!(systems[sys][component].includes(name))) systems[sys][component].push(name)
   }
 
-  for (const [key, val] of Object.entries(garagesObj)) {
-    console.log('Garage №', key)
-    val.forEach(el => console.log('---', el.split(': ').join(' - ')))
+  let sortedRegister = Object.entries(systems).sort((a, b) => {
+    if (Object.keys(b[1]).length - Object.keys(a[1]).length !== 0)
+      return Object.keys(b[1]).length - Object.keys(a[1]).length
+    return a[0].localeCompare(b[0])
+  })
+
+
+  for (const [key, val] of sortedRegister) {
+    console.log(key)
+    for (const [k, v] of Object.entries(val)) {
+      console.log(`|||${k}`)
+      v.sort((a, b) => b.length - a.length)  //TODO#........... 
+      .forEach(el => console.log(`||||||${el}`))
+    }
   }
 }
 
-// garage([
-//   '1 - color: blue, fuel type: diesel',
-//   '1 - color: red, manufacture: Audi',
-//   '2 - fuel type: petrol',
-//   '4 - color: dark blue, fuel type: diesel, manufacture: Fiat'
-// ])
+systemsRegister([
+  'SULS | Main Site | Home Page',
+  'SULS | Main Site | Login Page',
+  'SULS | Main Site | Register Page',
+  'SULS | Judge Site | Login Page',
+  'SULS | Judge Site | Submittion Page',
+  'Lambda | CoreA | A23',
+  'SULS | Digital Site | Login Page',
+  'Lambda | CoreB | B24',
+  'Lambda | CoreA | A24',
+  'Lambda | CoreA | A25',
+  'Lambda | CoreC | C4',
+  'Indice | Session | Default Storage',
+  'Indice | Session | Default Security'
+])
+
+
+
+
+
+
+
