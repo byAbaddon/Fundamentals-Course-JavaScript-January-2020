d7bf2f0839219a4e10bbdd69d9f84345e3f297b4
byAbbadon <kasiopess@abv.bg>
2021-03-25 17:29:29 +0200

    fix

--- 5.2 Objects and Classes - Exercise/10. SystemsRegiser.js
+++ 5.2 Objects and Classes - Exercise/10. SystemsRegiser.js
@@ -1,47 +1,31 @@
-function systemsRegister(input) {                   //copy paste
-  let systems = {}
+function systemsRegister(input) {
+    let systems = {}
 
-  for (let i = 0; i < input.length; i++) {
-      let [systemName, componentName, subcomponentName] = input[i].split(" | ")
-      if (!systems.hasOwnProperty(systemName)) {
-          systems[systemName] = {}
-      }
-      if (!(systems[systemName].hasOwnProperty(componentName))) {
-          systems[systemName][componentName] = []
-      }
-      if (!(systems[systemName][componentName].includes(subcomponentName))) {
-          systems[systemName][componentName].push(subcomponentName)
-      }
+    for (line of input) {
+        let [sys, component, name] = line.split(' | ')
+        if (!systems[sys]) systems[sys] = {}
+        if (!(systems[sys][component])) systems[sys][component] = []
+        if (!(systems[sys][component].includes(name))) systems[sys][component].push(name)
+    }
 
-  }
+    let sortedRegister = Object.entries(systems).sort((a, b) => {
+        if (Object.keys(b[1]).length - Object.keys(a[1]).length !== 0)
+            return Object.keys(b[1]).length - Object.keys(a[1]).length
+        return a[0].localeCompare(b[0])
+    })
 
-  let register = Object.entries(systems);
-  register.sort((a, b) => {   
-      if (Object.keys(b[1]).length - Object.keys(a[1]).length !== 0) {
-          return Object.keys(b[1]).length - Object.keys(a[1]).length
-      } else {               
-          return a[0].localeCompare(b[0])
-      }
-  });
+    sortedRegister.forEach((x, i) => {
+        sortedRegister[i][1] = Object.entries(x[1]).sort((a, b) => b[1].length - a[1].length)
+    })
 
-  register.forEach((x, i) => {                 
-      let components = Object.entries(x[1])
-      components.sort((a, b) => {
-          return b[1].length - a[1].length
-      });
-      register[i][1] = components
-  });
 
-  for (let i = 0; i < register.length; i++) {   
-      console.log(`${register[i][0]}`)
-      for (let j = 0; j < register[i][1].length; j++) {   
-          console.log(`|||${register[i][1][j][0]}`)
-          for (let s = 0; s < register[i][1][j][1].length; s++) {  
-              console.log(`||||||${register[i][1][j][1][s]}`)
-          }
-      }
-  }
-
+    for (const [key, val] of sortedRegister) {
+        console.log(key)
+        for (const [_, k] of Object.entries(val)) {
+            console.log(`|||${k[0]}`)
+            k[1].forEach(el => console.log(`||||||${el}`))
+        }
+    }
 }
 
 // systemsRegister([
@@ -58,5 +42,4 @@ function systemsRegister(input) {                   //copy paste
 //   'Lambda | CoreC | C4',
 //   'Indice | Session | Default Storage',
 //   'Indice | Session | Default Security'
-// ]
-// )
+// ])
