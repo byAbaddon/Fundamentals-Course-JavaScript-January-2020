6d9e1283180d90967d658526ccb8689f020fc2af
byAbaddon <51271834+byAbaddon@users.noreply.github.com>
2023-05-13 03:42:41 +0300

    Update 09. Login.js

--- 1.1 Basic-Syntax-Conditional-Statements-and-Loops-Lab-Exercise/09. Login.js
+++ 1.1 Basic-Syntax-Conditional-Statements-and-Loops-Lab-Exercise/09. Login.js
@@ -1,18 +1,15 @@
-function login(arg) {
-  let userName = arg.shift()
-  let reversedArr = arg.map(el => el.split('').reverse().join(''))
+function login(arr) {
+  let username = arr.shift();
+  let password = username.split('').reverse().join('')
+  let attempt = 0
+  let testPass = arr.shift()
 
-  for (let i = 0; i < reversedArr.length; i++) {
-    if (i >= 3) {
-      console.log(`User ${userName} blocked!`);
-      return
-    } else if (reversedArr[i] !== userName) {
-      console.log(`Incorrect password. Try again.`);
-    } else {
-      console.log(`User ${userName} logged in.`);
-
-    }
+  while (testPass != password && attempt++ < 3) {
+    console.log(`Incorrect password. Try again.`)
+    testPass = arr.shift()
   }
+
+  console.log(`User ${username} ${testPass == password ? 'logged in.' : 'blocked!'}`)
 }
 
 //login(['Acer','login','go','let me in','recA'])
